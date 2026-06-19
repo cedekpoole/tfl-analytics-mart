74ad69dc9d66c027464909ea1df6c8785cbfb2f2
cedekpoole <cameron.edek.poole@gmail.com>
2026-06-19 11:51:01 +0100

    add comments

--- fetch_tfl.py
+++ fetch_tfl.py
@@ -7,4 +7,5 @@ response = requests.get(url, timeout=30)
 response.raise_for_status()
 
 data = response.json()
+# json.dumps will convert the data to a JSON string and indent it for better readability
 print(json.dumps(data, indent=2))
