6d5d9513979e8fe6d0fbc943ba25c39310684db1
cedekpoole <cameron.edek.poole@gmail.com>
2026-06-21 19:39:26 +0100

    create first test

---
+++ tests/test_fetch_tfl.py
@@ -0,0 +1,21 @@
+import json
+
+from fetch_tfl import save_tfl_data
+
+
+def test_save_tfl_data(tmp_path):
+    # no need to call TfL API - create fake data
+    FAKE_DATA = [
+        {
+            "name": "Waterloo & City",
+            "lineStatuses": {"StatusSeverityUpdate": "Minor Delays"},
+        }
+    ]
+    # create temp file path
+    saved_file_path = save_tfl_data(FAKE_DATA, output_dir=tmp_path)
+
+    # open path and check if the saved data is the same as the fake data
+    with open(saved_file_path) as file:
+        saved_data = json.load(file)
+
+    assert FAKE_DATA == saved_data
