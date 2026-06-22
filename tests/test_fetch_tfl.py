2ec7a20c5bd7222d0779f649ae52deb1c8152682
cedekpoole <cameron.edek.poole@gmail.com>
2026-06-22 08:18:47 +0100

    create test for print_line_statuses function

--- tests/test_fetch_tfl.py
+++ tests/test_fetch_tfl.py
@@ -1,16 +1,17 @@
 import json
 
-from fetch_tfl import save_tfl_data
+from fetch_tfl import save_tfl_data, print_line_statuses
+
+# no need to call TfL API - create fake data
+FAKE_DATA = [
+    {
+        "name": "Waterloo & City",
+        "lineStatuses": [{"statusSeverityDescription": "Minor Delays"}],
+    }
+]
 
 
 def test_save_tfl_data(tmp_path):
-    # no need to call TfL API - create fake data
-    FAKE_DATA = [
-        {
-            "name": "Waterloo & City",
-            "lineStatuses": [{"StatusSeverityDescription": "Minor Delays"}],
-        }
-    ]
     # create temp file path
     saved_file_path = save_tfl_data(FAKE_DATA, output_dir=tmp_path)
 
@@ -19,3 +20,11 @@ def test_save_tfl_data(tmp_path):
         saved_data = json.load(file)
 
     assert FAKE_DATA == saved_data
+
+
+# capsys and tmp_path come from pytest itself (as fixtures)
+def test_print_line_statuses(capsys):
+    print_line_statuses(FAKE_DATA)
+    captured = capsys.readouterr()
+
+    assert captured.out == "Waterloo & City : Minor Delays\n"
